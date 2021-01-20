38b5741059d8c86b95c66cd725cedf58878bb317
nathantseng <tsnathan@umich.edu>
2021-01-19 22:35:20 -0500

    add multiscale loading

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ def load_args():
     # Dataloader params
     arg_parser.add_argument('--input_dir', default='datasets/road2icy')
     arg_parser.add_argument('--num_scales', default=3, type=int)
-    arg_parser.add_argument('--scale_factor', default=0.5, type=float)
+    arg_parser.add_argument('--scale_factor', default=2, type=int)
+    arg_parser.add_argument('--base_resolution', default=64, type=int)
 
     return arg_parser
 
--- util/dataloader.py
+++ util/dataloader.py
@@ -1,10 +1,7 @@
-from skimage import io
 from natsort import natsorted
 from PIL import Image
 from pathlib import Path
-from torchvision.utils import save_image
 import torch.utils.data
-import util.resize_right as resizer
 import matplotlib.pyplot as plt
 import os
 import numpy as np
@@ -15,16 +12,37 @@ class domain_dataloader():
         self.input_root = Path(opt.input_dir)
         self.scale_dirs = [self.input_root / Path("original")]
 
-        for scale_num in range(1, opt.num_scales+1):
-            output_scale_dir = self.input_root / Path("scale-{}".format(scale_num))
-            output_scale_dir.mkdir(parents=True, exist_ok=True)
-            self.scale_dirs.append(output_scale_dir)
+        initialized = create_scales(self.input_root, self.scale_dirs, opt)
+        if not initialized:
             source_B = natsorted(os.listdir(self.scale_dirs[0] / Path("trainB")))
             for item in source_B:
                 path = self.scale_dirs[0] / Path("trainB") / Path(item)
-                if os.path.isfile(str(path)):
-                    image = Image.open(str(path)).convert("RGB")
-                    im = image.resize((128, 128), Image.ANTIALIAS)
-                    im.save("test"+item)
+                create_scaled_variants(path, self.scale_dirs, opt)
 
 
+
+def create_scales(input_root, scale_dirs, opt):
+    initialized = True
+    for scale_num in range(1, opt.num_scales + 1):
+        output_scale_dir = input_root / Path("scale-{}".format(scale_num))
+        if not os.path.isdir(output_scale_dir):
+            initialized = False
+            output_scale_dir.mkdir(parents=True, exist_ok=True)
+            dir_A = output_scale_dir / Path("trainA")
+            dir_A.mkdir(parents=True, exist_ok=True)
+            dir_B = output_scale_dir / Path("trainB")
+            dir_B.mkdir(parents=True, exist_ok=True)
+        scale_dirs.append(output_scale_dir)
+    return initialized
+
+
+def create_scaled_variants(path, scale_dirs, opt):
+    if os.path.isfile(str(path)):
+        original = Image.open(str(path)).convert("RGB")
+        scale_res = opt.base_resolution
+        for scale, dir in enumerate(scale_dirs):
+            if scale != 0:
+                im = original.resize((scale_res, scale_res), Image.ANTIALIAS)
+                output_path = dir / Path("trainB") / os.path.basename(path)
+                im.save(output_path)
+                scale_res *= opt.scale_factor
